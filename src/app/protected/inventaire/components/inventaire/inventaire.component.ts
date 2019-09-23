a836e429d76da55f25383d746d06a665b7b4f3bb
thierrytrimintin <thierry.trimintin@hotmail.fr>
2019-09-23 13:25:06 +0200

    ''

--- src/app/protected/inventaire/components/inventaire/inventaire.component.ts
+++ src/app/protected/inventaire/components/inventaire/inventaire.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Inventaire, Article } from 'src/app/shared/models/model';
@@ -84,6 +85,7 @@ export class InventaireComponent implements OnInit {
 
 
   // Méthodes pour l'affichage
+  
   compteManquant(tab: Article[]): number {
     let cpt = 0;
     tab.forEach(article => {
