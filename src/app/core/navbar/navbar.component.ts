3c1ecd6104fa0ed134a915157a511df48599a2a8
ncbtart@gmail.com <ncbtart@gmail.com>
2019-10-08 13:54:52 +0200

    disconnect

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavBarComponent { }
+export class NavBarComponent {
+  constructor(private auth: AuthService,
+              private router: Router) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigateByUrl('/auth');
+  }
+ }
